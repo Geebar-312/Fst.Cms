3fe8ad905aeb2abb8b8abfbb3d0414901e173ff8
Geebar_312 <3399457143@qq.com>
2024-03-22 00:05:48 +0800

    ”新增之前的学习代码“

---
+++ Sample01/Controllers/ContentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        /*public IActionResult Index()
+        {
+            var contents = new List<Content>();
+            for (int i = 1; i < 11; i++)
+            {
+                contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            }
+            return View(new ContentViewModel { Contents = contents });
+
+        }*/
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+}
---
+++ Sample01/Models/ContentViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample01.Models
+{
+    public class ContentViewModel
+    {
+        /// <summary>
+        /// 内容列表
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+}
